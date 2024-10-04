c8aa89ea966660c4b7f51b16633fedb3ca8c2edc
Serg Puhoff <serg@puhoff.com>
2024-10-04 22:07:46 +0400

    add test and validation

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,5 +10,5 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of :content }
 end
