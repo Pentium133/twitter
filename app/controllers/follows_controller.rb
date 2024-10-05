1287496fc02e775500c1be4f503636820440e967
Serg Puhoff <serg@puhoff.com>
2024-10-05 13:18:46 +0400

    add post feed

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -4,12 +4,12 @@ class FollowsController < ApplicationController
   def create
     user = User.find(params[:user_id])
     current_user.follow_to(user)
-    redirect_to user_path(user), notice: "You are now following #{user.email}."
+    redirect_to root_path, notice: "You are now following #{user.email}."
   end
 
   def destroy
     user = User.find(params[:user_id])
     current_user.unfollow_from(user)
-    redirect_to user_path(user), notice: "You have unfollowed #{user.email}."
+    redirect_to root_path, notice: "You have unfollowed #{user.email}."
   end
 end
--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,5 +1,7 @@
 class PageController < ApplicationController
   before_action :authenticate_user!
   def home
+    following_users = current_user.following.includes(:user).map(&:user)
+    @posts = Post.where(user: following_users).includes(:user).order(created_at: :desc)
   end
 end
